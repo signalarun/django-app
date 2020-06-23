eb7476877aac52caea8f48081a2573ae31571f86
signalarun <signalarun@gmail.com>
2020-06-23 16:24:41 +0530

    update : model book

--- dapp/listings/models.py
+++ dapp/listings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
+    distributor = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    def __str__(self):
+        return self.title
